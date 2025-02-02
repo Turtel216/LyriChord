7587c9ffc1ac797c5e4ace0745920aeecc3b8001
Turtel216 <papadimitrios216@gmail.com>
2025-02-02 13:00:46 +0200

    updated utils documentation

--- internal/utils/string.go
+++ internal/utils/string.go
@@ -1,12 +1,16 @@
 package utils
 
+// SplitStringIntoChunks splits a given string `s` into smaller chunks of at most `maxLen` characters.
+// It returns a slice of strings containing the chunks.
+// If `maxLen` is less than or equal to zero, it returns a slice with an error message.
+// If `s` is empty, it returns a slice containing an empty string.
 func SplitStringIntoChunks(s string, maxLen int) []string {
 	if maxLen <= 0 {
 		return []string{"Invalid max length"}
 	}
 
 	if s == "" {
-		return []string{""}
+		return []string{s}
 	}
 
 	var result []string
